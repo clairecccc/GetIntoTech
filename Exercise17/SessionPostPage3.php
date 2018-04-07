0bdd843a7841a0ac4f6ee93ab6c7f5a1bcf7fa9a
Claire <clairecunliffe@gmail.com>
2018-04-07 17:50:51 +0100

    Tidy up code formatting

--- Exercise17/SessionPostPage3.php
+++ Exercise17/SessionPostPage3.php
@@ -6,11 +6,10 @@ $_SESSION =[];
 session_destroy();
 
 print_r($_SESSION);
+
 if(empty($_SESSION)){
     echo "Welcome Guest";
 }
 
-echo "<a href='SessionPostPage1.php'>Go to Page 1: Login Page</a><br>";
-
-
+echo "<a href='SessionPostPage1.php'>Go to Page 1: Click here to login</a><br>";
 ?>
